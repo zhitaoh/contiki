a2f9d916bf0ab6a50d6884c4d160646f8895719d
Zhitao He <zhitao.he@assaabloy.com>
2018-09-18 14:02:29 +0200

    change3 defaults to one second pkt intervals and 100 pkts

--- apps/cc2420-jammer/cc2420-jammer.c
+++ apps/cc2420-jammer/cc2420-jammer.c
@@ -73,8 +73,8 @@
 #define CHANNEL 20
 #define TXPOWER_LEVEL 3
 
-#define TX_INTERVAL (CLOCK_SECOND / 32)
-#define MAX_TX_PACKETS 10
+#define TX_INTERVAL (CLOCK_SECOND / 1)
+#define MAX_TX_PACKETS 100
 #define PAYLOAD_LEN 20
 /* exprimental value used to occupy near full bandwidth, assuming RIMTER_SECOND = 4096 * N */
 /* "SFD gap" = 298 us out of 4395 us Droplet interval => actual free air time = 298 - 160 (preamble+SFD) = 138 us => free bandwidth = 138/4395 = 3.1% */
